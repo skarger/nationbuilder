b063e8f41d2cf2b8a803a921e986bf7ed0bf87ea
Stephen Karger <stephen.karger@gmail.com>
2018-02-19 22:44:54 -0500

    Test main program wrapper

--- developer_exercises/app/main.rb
+++ developer_exercises/app/main.rb
@@ -1,3 +1,12 @@
-def main
+require "logger"
+
+def main(logger: Logger.new($stderr))
+  logger.info("NationBuilder Developer Exercises: Starting")
+
   0
+end
+
+
+if $PROGRAM_NAME == __FILE__
+  main
 end
--- developer_exercises/spec/main_spec.rb
+++ developer_exercises/spec/main_spec.rb
@@ -1,7 +1,18 @@
 require File.expand_path("../../app/main.rb", __FILE__)
 
 describe "main" do
+  let(:logger) { Logger.new(File.expand_path("../../app/log/test.log", __FILE__)) }
+
   it "returns a successful exit code" do
-    expect(main).to eq(0)
+    expect(main(logger: logger)).to eq(0)
+  end
+
+  it "creates an application logger by default" do
+    allow(logger).to receive(:info)
+    allow(Logger).to receive(:new).with($stderr).and_return(logger)
+
+    main
+
+    expect(logger).to have_received(:info).with("NationBuilder Developer Exercises: Starting")
   end
 end
