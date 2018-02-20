ed11d45fd1f3434bc1d318a9ef2775056930f12e
Stephen Karger <stephen.karger@gmail.com>
2018-02-19 22:53:51 -0500

    Check for API Token

--- developer_exercises/app/main.rb
+++ developer_exercises/app/main.rb
@@ -3,7 +3,14 @@ require "logger"
 def main(logger: Logger.new($stderr))
   logger.info("NationBuilder Developer Exercises: Starting")
 
-  0
+  if ENV['NB_API_TOKEN'].nil? || ENV['NB_API_TOKEN'].empty?
+    logger.warn("ENV['NB_API_TOKEN'] unset. Exiting.")
+    1
+  else
+    0
+  end
+ensure
+  logger.info("NationBuilder Developer Exercises: Finished")
 end
 
 
--- developer_exercises/spec/main_spec.rb
+++ developer_exercises/spec/main_spec.rb
@@ -4,15 +4,27 @@ describe "main" do
   let(:logger) { Logger.new(File.expand_path("../../app/log/test.log", __FILE__)) }
 
   it "returns a successful exit code" do
+    ENV['NB_API_TOKEN'] = 'abc'
     expect(main(logger: logger)).to eq(0)
   end
 
   it "creates an application logger by default" do
+    ENV['NB_API_TOKEN'] = 'abc'
     allow(logger).to receive(:info)
     allow(Logger).to receive(:new).with($stderr).and_return(logger)
 
     main
 
     expect(logger).to have_received(:info).with("NationBuilder Developer Exercises: Starting")
+  end
+
+  it "errors out if no API token set" do
+    ENV['NB_API_TOKEN'] = nil
+    allow(logger).to receive(:warn)
+    allow(Logger).to receive(:new).with($stderr).and_return(logger)
+
+    expect(main(logger: logger)).to eq(1)
+
+    expect(logger).to have_received(:warn).with("ENV['NB_API_TOKEN'] unset. Exiting.")
   end
 end
